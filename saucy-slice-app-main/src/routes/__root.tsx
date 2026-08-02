4c6d5130e96de9f1015fca4371584698fca79d6b
Rayyan Hafeez <rayyanhafeeez@gmail.com>
2026-08-02 23:38:16 +0500

    new favicon.

--- saucy-slice-app-main/src/routes/__root.tsx
+++ saucy-slice-app-main/src/routes/__root.tsx
@@ -87,7 +87,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
     ],
   }),
   shellComponent: RootShell,
@@ -115,7 +115,6 @@ function RootComponent() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
       <Outlet />
     </QueryClientProvider>
   );
